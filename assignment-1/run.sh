447ce8eaad689b2fb670b7a8f997ae065abafde3
Janus Roestenburg <JanusRoestenburg@gmail.com>
2023-09-20 11:44:44 +0200

    fixed merge errors

--- assignment-1/run.sh
+++ assignment-1/run.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/bin/bash
 
 docker build -t image/shortlived-container .
@@ -9,16 +8,4 @@ docker run \
   --volume "$(pwd)/assignment":/usr/src/assignment\
   --network assignment-1-network\
   shortlived-container
-=======
-#!/bin/bash
-
-docker build -t image/shortlived-container .
-
-docker run \
-  -d --rm \
-  --name shortlived-container \
-  --volume "$(pwd)/assignment":/usr/src/assignment\
-  --network assignment-1-network\
-  shortlived-container
->>>>>>> 131ea354a20034e3061a3247d9116179a7cd4f5f
   
