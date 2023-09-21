1ea982eac92450d8437f2b3c29dddd7996904409
Janus Roestenburg <JanusRoestenburg@gmail.com>
2023-09-21 13:04:03 +0200

    u

--- assignment-1/run.sh
+++ assignment-1/run.sh
@@ -4,7 +4,7 @@ docker run \
   -d --rm \
   --name shortlived-container \
   --volume "$(pwd)/assignment":/usr/src/assignment \
-  --volume /var/run/docker.sock:/var/run/docker.sock \  # Mount the Docker socket
+  --volume /var/run/docker.sock:/var/run/docker.sock \
   --network assignment-1-network \
   image/shortlived-container:latest
 
