2779d3f468038fa8ddba7e04d55482f6ad1bde82
Janus Roestenburg <JanusRoestenburg@gmail.com>
2023-09-21 11:45:06 +0200

    update

--- assignment-1/entrypoint.sh
+++ assignment-1/entrypoint.sh
@@ -9,6 +9,6 @@ echo | curl -X 'POST' \
       "experiment_id": "5678",
       "cipher_data": "D5qnEHeIrTYmLwYX.hSZNb3xxQ9MtGhRP7E52yv2seWo4tUxYe28ATJVHUi0J++SFyfq5LQc0sTmiS4ILiM0/YsPHgp5fQKuRuuHLSyLA1WR9YIRS6nYrokZ68u4OLC4j26JW/QpiGmAydGKPIvV2ImD8t1NOUrejbnp/cmbMDUKO1hbXGPfD7oTvvk6JQVBAxSPVB96jDv7C4sGTmuEDZPoIpojcTBFP2xA"
   }'\
-  >> ../assignment/log.txt
+  >> ../assignment2/log.txt
 
 exit
--- assignment-1/run.sh
+++ assignment-1/run.sh
@@ -5,7 +5,7 @@ docker build -t image/shortlived-container:latest .
 docker run \
   -d --rm \
   --name shortlived-container \
-  --volume "$(pwd)/assignment":/usr/src/assignment\
+  --volume "$(pwd)/assignment2":/usr/src/assignment2\
   --network assignment-1-network\
   image/shortlived-container:latest
 
