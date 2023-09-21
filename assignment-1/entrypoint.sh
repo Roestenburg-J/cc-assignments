050bb460e278dfd6ff398ac5b4d33d4647c81f7e
Janus Roestenburg <JanusRoestenburg@gmail.com>
2023-09-21 22:07:38 +0200

    Added debigging

--- assignment-1/entrypoint.sh
+++ assignment-1/entrypoint.sh
@@ -8,17 +8,19 @@ log_error() {
 }
 
 # Execute a curl command to communicate with the "notifications-service" container
-curl_output=$(curl -X 'POST' \
-  'http://notifications-service:3000/api/notify' \
-  -H 'accept: text/plain; charset=utf-8' \
-  -H 'Content-Type: application/json; charset=utf-8' \
-  -d '{
-      "notification_type": "OutOfRange",
-      "researcher": "d.landau@uu.nl",
-      "measurement_id": "1234",
-      "experiment_id": "5678",
-      "cipher_data": "D5qnEHeIrTYmLwYX.hSZNb3xxQ9MtGhRP7E52yv2seWo4tUxYe28ATJVHUi0J++SFyfq5LQc0sTmiS4ILiM0/YsPHgp5fQKuRuuHLSyLA1WR9YIRS6nYrokZ68u4OLC4j26JW/QpiGmAydGKPIvV2ImD8t1NOUrejbnp/cmbMDUKO1hbXGPfD7oTvvk6JQVBAxSPVB96jDv7C4sGTmuEDZPoIpojcTBFP2xA"
-  }')
+# curl_output=$(curl -X 'POST' \
+#   'http://notifications-service:3000/api/notify' \
+#   -H 'accept: text/plain; charset=utf-8' \
+#   -H 'Content-Type: application/json; charset=utf-8' \
+#   -d '{
+#       "notification_type": "OutOfRange",
+#       "researcher": "d.landau@uu.nl",
+#       "measurement_id": "1234",
+#       "experiment_id": "5678",
+#       "cipher_data": "D5qnEHeIrTYmLwYX.hSZNb3xxQ9MtGhRP7E52yv2seWo4tUxYe28ATJVHUi0J++SFyfq5LQc0sTmiS4ILiM0/YsPHgp5fQKuRuuHLSyLA1WR9YIRS6nYrokZ68u4OLC4j26JW/QpiGmAydGKPIvV2ImD8t1NOUrejbnp/cmbMDUKO1hbXGPfD7oTvvk6JQVBAxSPVB96jDv7C4sGTmuEDZPoIpojcTBFP2xA"
+#   }')
+
+curl_output=$(docker network inspect assignment-1-network)
 
 # Check the exit status of the curl command
 if [ $? -ne 0 ]; then
