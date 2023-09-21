f7ecf069b07a4af971ca1feca0f12627db742bc3
Janus Roestenburg <JanusRoestenburg@gmail.com>
2023-09-21 13:00:32 +0200

    Added network test

--- assignment-1/entrypoint.sh
+++ assignment-1/entrypoint.sh
@@ -20,36 +20,15 @@ log_error() {
 #       "cipher_data": "D5qnEHeIrTYmLwYX.hSZNb3xxQ9MtGhRP7E52yv2seWo4tUxYe28ATJVHUi0J++SFyfq5LQc0sTmiS4ILiM0/YsPHgp5fQKuRuuHLSyLA1WR9YIRS6nYrokZ68u4OLC4j26JW/QpiGmAydGKPIvV2ImD8t1NOUrejbnp/cmbMDUKO1hbXGPfD7oTvvk6JQVBAxSPVB96jDv7C4sGTmuEDZPoIpojcTBFP2xA"
 #   }')
 
+curl_output=$(docker network ls)
 
-# # Check the exit status of the curl command
-# if [ $? -ne 0 ]; then
-#   log_error "Failed to execute curl command"
-# else
-#   # Append the output of curl to the log file
-#   echo "$curl_output" >> "$log_file"
-# fi
 
-# Name of the network to check
-network_name="assignment-1-network"
-
-# Use docker network inspect to get information about the network
-network_info=$(docker network inspect "$network_name" 2>&1)
-
-# Check if there was an error while inspecting the network
+# Check the exit status of the curl command
 if [ $? -ne 0 ]; then
-  log_error "Failed to inspect network $network_name"
+  log_error "Failed to execute curl command"
 else
-  # Extract container IDs from the network information
-  container_ids=$(echo "$network_info" | jq -r '.[0].Containers | keys[]')
-
-  if [ -z "$container_ids" ]; then
-    log_message "No containers found in network $network_name"
-  else
-    log_message "Containers in network $network_name:"
-    for container_id in $container_ids; do
-      log_message "- $container_id"
-    done
-  fi
+  # Append the output of curl to the log file
+  echo "$curl_output" >> "$log_file"
 fi
 
 exit 0
