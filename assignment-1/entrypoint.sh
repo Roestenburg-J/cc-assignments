702141df72819f0c5473c65f1cb4908bbf6963fc
Janus Roestenburg <JanusRoestenburg@gmail.com>
2023-09-21 11:55:15 +0200

    Added error catching to entrypoint

--- assignment-1/entrypoint.sh
+++ assignment-1/entrypoint.sh
@@ -1,4 +1,14 @@
-echo | curl -X 'POST' \
+#!/bin/sh
+
+log_file="../assignment/log.txt"
+
+# Function to log errors
+log_error() {
+  echo "Error: $1" >> "$log_file"
+}
+
+# Execute the curl command
+curl_output=$(curl -X 'POST' \
   'http://notifications-service:3000/api/notify' \
   -H 'accept: text/plain; charset=utf-8' \
   -H 'Content-Type: application/json; charset=utf-8' \
@@ -8,7 +18,14 @@ echo | curl -X 'POST' \
       "measurement_id": "1234",
       "experiment_id": "5678",
       "cipher_data": "D5qnEHeIrTYmLwYX.hSZNb3xxQ9MtGhRP7E52yv2seWo4tUxYe28ATJVHUi0J++SFyfq5LQc0sTmiS4ILiM0/YsPHgp5fQKuRuuHLSyLA1WR9YIRS6nYrokZ68u4OLC4j26JW/QpiGmAydGKPIvV2ImD8t1NOUrejbnp/cmbMDUKO1hbXGPfD7oTvvk6JQVBAxSPVB96jDv7C4sGTmuEDZPoIpojcTBFP2xA"
-  }'\
-  >> ./assignment/log.txt
+  }')
 
-exit
+# Check the exit status of the curl command
+if [ $? -ne 0 ]; then
+  log_error "Failed to execute curl command"
+else
+  # Append the output of curl to the log file
+  echo "$curl_output" >> "$log_file"
+fi
+
+exit 0
