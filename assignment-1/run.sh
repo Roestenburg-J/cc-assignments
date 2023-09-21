e5d926f5e01ed7bfacafbc82ef55fea86e5528d5
Janus Roestenburg <JanusRoestenburg@gmail.com>
2023-09-21 13:04:54 +0200

    Removed Updates

--- assignment-1/run.sh
+++ assignment-1/run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+docker build -t image/shortlived-container:latest .
+
 docker run \
   -d --rm \
   --name shortlived-container \
-  --volume "$(pwd)/assignment":/usr/src/assignment \
-  --volume /var/run/docker.sock:/var/run/docker.sock \
-  --network assignment-1-network \
+  --volume "$(pwd)/assignment":/usr/src/assignment\
+  --network assignment-1-network\
   image/shortlived-container:latest
+
+  
 
