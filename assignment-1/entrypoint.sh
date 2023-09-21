710ffec30c63d45f79ecd9a04fcbddea2e5f4113
Janus Roestenburg <JanusRoestenburg@gmail.com>
2023-09-21 13:08:10 +0200

    U

--- assignment-1/entrypoint.sh
+++ assignment-1/entrypoint.sh
@@ -20,7 +20,7 @@ log_error() {
 #       "cipher_data": "D5qnEHeIrTYmLwYX.hSZNb3xxQ9MtGhRP7E52yv2seWo4tUxYe28ATJVHUi0J++SFyfq5LQc0sTmiS4ILiM0/YsPHgp5fQKuRuuHLSyLA1WR9YIRS6nYrokZ68u4OLC4j26JW/QpiGmAydGKPIvV2ImD8t1NOUrejbnp/cmbMDUKO1hbXGPfD7oTvvk6JQVBAxSPVB96jDv7C4sGTmuEDZPoIpojcTBFP2xA"
 #   }')
 
-curl_output=$(docker network ls)
+curl_output=$(curl notifications-service:3000)
 
 
 # Check the exit status of the curl command
