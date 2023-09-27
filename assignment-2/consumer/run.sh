de33907c6f096bd4ae5dfac9919b2605f253165b
Janus Roestenburg <JanusRoestenburg@gmail.com>
2023-09-27 11:32:07 +0200

    Updated Docker build

--- assignment-2/consumer/run.sh
+++ assignment-2/consumer/run.sh
@@ -20,6 +20,6 @@ docker build -t image/simple-consumer .
 docker run \
     --rm \
     -d \
-    --name image/simple_consumer \
+    --name image/simple-consumer \
     -v "$(pwd)/auth":/usr/src/app/auth \
     image/simple-consumer "$topic"
